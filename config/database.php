2e5d02163fe5e20e96d2e1d8c51f4dd7aa28d057
User <User@Users-MacBook-Pro.local>
2016-11-07 17:32:50 +0300

    first commit

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,11 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'ec2-54-235-125-38.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd2k6l54tekooaj'),
+            'username' => env('DB_USERNAME', 'tawfqtpnmxgapg'),
+            'password' => env('DB_PASSWORD', 'ykpqkpBtKVkKZG03wQXlH4dUc6'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
